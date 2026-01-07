7fe064942af0f8a00bc1d1d43ce7e990bffe6dab
sevengen <sevengenautomacao@gmail.com>
2026-01-07 15:02:01 +0000

    na hora de lancar pontuacao eu quero que a data fique alinhada com o dia

--- src/components/generate-score-form.tsx
+++ src/components/generate-score-form.tsx
@@ -151,9 +151,6 @@ export default function GenerateScoreForm({ members, scoringCriteria, onScoresCa
                     mode="single"
                     selected={field.value}
                     onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
                     initialFocus
                     locale={ptBR}
                   />
