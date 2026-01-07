6b16401a93b103353b60f494ce35fc46ef31ff4b
sevengen <sevengenautomacao@gmail.com>
2026-01-07 14:29:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8072151103-82d92",
+  "appId": "1:603016801954:web:3f9626bb9ef73c2a09f1fb",
+  "apiKey": "AIzaSyBA0PPGvZSfMDNv8P5joh-jZK91QumxWBI",
+  "authDomain": "studio-8072151103-82d92.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "603016801954"
+};
